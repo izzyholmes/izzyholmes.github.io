4d703b178795de4a73f23b3b6f498627af72f2de
izzyholmes <izzy@izzyholmes.co>
2024-10-15 17:35:48 -0500

    Update behavior.js

--- behavior.js
+++ behavior.js
@@ -1,5 +1,5 @@
 (function(){
-  const button=document.getElementById('button')'
+  const button=document.getElementById('button')
     button.addEventListener('click',function(){
       alert('Let's see if this button works');
     });
